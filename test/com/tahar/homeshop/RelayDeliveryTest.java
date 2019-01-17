1af1a2c4fb35f63b0d1d369aa902caf810276688
tahar26 <taharraaf@gmail.com>
2019-01-17 20:34:20 +0100

    toto

---
+++ test/com/tahar/homeshop/RelayDeliveryTest.java
@@ -0,0 +1,26 @@
+package com.tahar.homeshop;
+
+public class RelayDeliveryTest {
+
+    public class RelayDeliveryTest {
+
+        @Test
+        public void Given_FreeRelay_WhenGettingDeliveryPrice_ThenGet0e() {
+            Delivery delivery = new RelayDelivery(5);
+            assertEquals(0.0, delivery.getPrice(), 0.01);
+        }
+
+        @Test
+        public void Given_LowPriceRelay_WhenGettingDeliveryPrice_ThenGet2e99() {
+            Delivery delivery = new RelayDelivery(27);
+            assertEquals(2.99, delivery.getPrice(), 0.01);
+        }
+
+        @Test
+        public void Given_HighPriceRelay_WhenGettingDeliveryPrice_ThenGet4e99() {
+            Delivery delivery = new RelayDelivery(52);
+            assertEquals(4.99, delivery.getPrice(), 0.01);
+        }
+
+    }
+}
